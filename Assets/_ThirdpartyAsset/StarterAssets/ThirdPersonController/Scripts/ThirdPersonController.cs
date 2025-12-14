cb3fc641f689e3fb993230fa4ebde65114ec0c94
NonthawatKRT <135787256+NonthawatKRT@users.noreply.github.com>
2025-12-15 01:24:40 +0700

    Hot Fix sound error

--- Assets/_ThirdpartyAsset/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/_ThirdpartyAsset/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -129,7 +129,7 @@ namespace StarterAssets
             {
                 base.OnSpawned();
 
-                enabled = isOwner;
+                // enabled = isOwner;
 
                 if (!isOwner)
                 {
